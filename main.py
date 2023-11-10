35d842564585def0c82df09fcd0c11c5cdd50eac
WyattjkRichard <wyatt@rhouse.me>
2023-11-10 15:15:02 -0500

    Moved TLE init into a function, added control for GPIO with PI

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ import json
 import geocoder
 import ephem
 from datetime import datetime
+import RPi.GPIO as GPIO
+import time
 
-TLE_URL = "https://tle.ivanstanojevic.me/api/tle/25544"
+
+TLE_URL = "https://tle.ivanstanojevic.me/api/tle/"
+SAT_ID = 25544
+
+# "name":"ISS (ZARYA)"
+# "line1":"1 25544U 98067A   23314.24517226  .00093162  00000+0  16505-2 0  9991"
+# "line2":"2 25544  51.6416 325.0671 0002415 274.2971 187.0056 15.49490004424444"
 
 
 def jprint(obj):
@@ -13,20 +21,46 @@ def jprint(obj):
     print(text)
 
 
+def init_observer(sat_id):
+    response = requests.get(f"{TLE_URL}{sat_id}")
+    # ideally, check response to ensure no issues
+    tle_name = response.json()['name']
+    tle_line1 = response.json()['line1']
+    tle_line2 = response.json()['line2']
+    satellite = ephem.readtle(tle_name, tle_line1, tle_line2)
+    sat_dish = ephem.Observer()
+    sat_dish.lat, sat_dish.long = geocoder.ip('me').latlng
+    return sat_dish, satellite
+
+
+def gpio_test():
+    GPIO.setmode(GPIO.BOARD)
+    control_pins = [7, 11, 13, 15]
+    for pin in control_pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, 0)
+    halfstep_seq = [
+        [1, 0, 0, 0],
+        [1, 1, 0, 0],
+        [0, 1, 0, 0],
+        [0, 1, 1, 0],
+        [0, 0, 1, 0],
+        [0, 0, 1, 1],
+        [0, 0, 0, 1],
+        [1, 0, 0, 1]
+    ]
+    for i in range(512):
+        for halfstep in range(8):
+            for pin in range(4):
+                GPIO.output(control_pins[pin], halfstep_seq[halfstep][pin])
+        time.sleep(0.001)
+    GPIO.cleanup()
+
+
 if __name__ == '__main__':
-    response = requests.get(TLE_URL)
-    jprint(response.json())
-
-    TLEName = response.json()['name']
-    TLELine1 = response.json()['line1']
-    TLELine2 = response.json()['line2']
-    sat = ephem.readtle(TLEName, TLELine1, TLELine2)
-
-    satDish = ephem.Observer()
-    satDish.lat, satDish.long = geocoder.ip('me').latlng
-    satDish.date = datetime.now()
-
+    satDish, sat = init_observer(SAT_ID)
+    satDish.date = datetime.utcnow()
     sat.compute(satDish)
-
     print(sat.az)
     print(sat.alt)
+    gpio_test()
